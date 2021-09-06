0a2116cc866de2026717e2648a854df8fb7159f8
tanmaygoyal258 <80524126+tanmaygoyal258@users.noreply.github.com>
2021-09-06 20:28:35 +0530

    Update code.py

--- Assignment5/code.py
+++ Assignment5/code.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+coeff = np.array([5**0.5 , 1 , 5**0.5])
+print(np.roots(coeff))
+
 x = np.linspace(-10,10,10000)
 x_axis = np.zeros((10000,))
-def f(x):
-    return (5**0.5)*(x**2) + x + (5**0.5)
 
-plt.plot(x , f(x) , label = "$\\sqrt{5}x^2 + x + \sqrt{5}$")
+
+plt.plot(x , coeff[0]*x**2 + coeff[1]*x + coeff[2] , label = "$\\sqrt{5}x^2 + x + \sqrt{5}$")
 plt.plot(x , x_axis , label ="$ y = 0 $")
 plt.grid(True)
 plt.legend()
