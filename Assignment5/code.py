fcc1b910a9e2fda8a1c4ca249da1f79e292d678c
tanmaygoyal258 <80524126+tanmaygoyal258@users.noreply.github.com>
2021-09-12 13:30:31 +0530

    Update code.py

--- Assignment5/code.py
+++ Assignment5/code.py
@@ -1,17 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from numpy import linalg as LA
 
 coeff = np.array([5**0.5 , 1 , 5**0.5])
 print(np.roots(coeff))
 
-x = np.linspace(-10,10,10000)
-x_axis = np.zeros((10000,))
+len = 100
+y = np.linspace(-4,4,len)
+
+def parab_gen(y,a):
+    x = y**2/a
+    return x/4
+
+V = np.array(([5**0.5 , 0],[0,0]))
+u = np.array(([0.5,-0.5]))
+f = 5**0.5
+
+D_vec,P = LA.eig(V)
+D = np.diag(D_vec)
 
 
-plt.plot(x , coeff[0]*x**2 + coeff[1]*x + coeff[2] , label = "$\\sqrt{5}x^2 + x + \sqrt{5}$")
-plt.plot(x , x_axis , label ="$ y = 0 $")
-plt.grid(True)
-plt.legend()
-plt.ylim(-5,20)
+p = P[:,1]
+
+eta = u@p
+foc = -(2 * eta)/D_vec[0]
+
+x = parab_gen(y,foc)
+
+cA = np.vstack((u+eta*p,V))
+cb = np.vstack((-f,(eta*p-u).reshape(-1,1)))
+c = LA.lstsq(cA,cb,rcond=None)[0]
+c = c.flatten()
+
+xStandardparab = np.vstack((y,x))
+
+xActualparab = P@xStandardparab + c[:,np.newaxis]
+
+
+plt.plot(xActualparab[0,:],xActualparab[1,:],label='$\\sqrt{5}x^2 + x + \\sqrt{5}$',color='r')
+plt.plot([i for i in range(-5,5)] , [0 for i in range(-5,5)] , 'k-',label = "x-axis" )
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='upper right')
+plt.grid() 
+
 plt.show()
