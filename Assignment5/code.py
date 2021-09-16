1d247c353f7b9186138df6be2dff388e5cd1adfb
tanmaygoyal258 <80524126+tanmaygoyal258@users.noreply.github.com>
2021-09-16 10:41:57 +0530

    Update code.py

--- Assignment5/code.py
+++ Assignment5/code.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
+import timeit
 
 coeff = np.array([5**0.5 , 1 , 5**0.5])
 print(np.roots(coeff))
@@ -27,10 +28,26 @@ foc = -(2 * eta)/D_vec[0]
 
 x = parab_gen(y,foc)
 
+start = timeit.timeit()
+cA = np.block([[u+eta*p],[V]])
+cb = np.block([[-f],[(eta*p-u).reshape(-1,1)]])
+c = LA.lstsq(cA,cb,rcond=None)[0]
+c = c.flatten()
+end = timeit.timeit()
+time1 = end - start
+
+start = timeit.timeit()
 cA = np.vstack((u+eta*p,V))
 cb = np.vstack((-f,(eta*p-u).reshape(-1,1)))
 c = LA.lstsq(cA,cb,rcond=None)[0]
 c = c.flatten()
+end = timeit.timeit()
+time2 = end - start
+
+if time1>time2:
+    print("np.vstack is more efficient than np.block")
+else:
+    print("np.block is more efficient than np.vstack")
 
 xStandardparab = np.vstack((y,x))
 
